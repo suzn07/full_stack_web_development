782ba0a95e63bcf1b174ce5302fdff015f0fe9fa
suzn077 <baralisujan101@gmail.com>
2025-10-12 15:21:13 +0300

    task07

---
+++ assignment/Js/module 2/task07/task07.js
@@ -0,0 +1,27 @@
+function rollDice(sides) {
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+function main() {
+  const resultsList = document.querySelector(".dicerolls");
+  resultsList.innerHTML = "";
+
+  const sides = Number(prompt("Enter the number of sides on the dice:"));
+  if (sides < 1) {
+    alert("The dice must have at least 1 side!");
+    return;
+  }
+
+  const maxNumber = sides;
+  let roll;
+  let html = "";
+
+  while (roll !== maxNumber) {
+    roll = rollDice(sides);
+    html += `<li>You rolled: ${roll}</li>`;
+  }
+
+  resultsList.innerHTML = html;
+}
+
+main();
